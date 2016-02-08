b3459e1e5873ad2518c63deffce39cead7656d57
cdrobin <cdrewrobin@gmail.com>
2016-02-08 11:33:01 -0500

    Changes to enable fragments to interact

--- app/src/main/java/edu/dtcc/crobin57/fragments/InputFragment.java
+++ app/src/main/java/edu/dtcc/crobin57/fragments/InputFragment.java
@@ -1,5 +1,6 @@
 package edu.dtcc.crobin57.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,23 +12,54 @@ import android.widget.EditText;
 
 public class InputFragment extends Fragment {
 
+    // Fields
     private static EditText weightInput;
+
+    // Interface to interact with activity
+    public interface InFragListener {
+        void focusChanged(String weight);
+    }
+
+    InFragListener activityCommander;
+
+    // Handler called when fragment is attached to activity
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // Initialize interface object
+        try {
+            activityCommander = (InFragListener) context;
+        }
+        catch (ClassCastException e) {
+            throw new ClassCastException(context.toString());
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout
         View view = inflater.inflate(R.layout.input_fragment, container, false);
 
+        // Get reference to weightInput
         weightInput = (EditText) view.findViewById(R.id.weightInput);
 
+        // Focus is changed listener
         weightInput.setOnFocusChangeListener(
             new View.OnFocusChangeListener() {
                 public void onFocusChange(View v, boolean hasFocus) {
-
+                    focusChanged(v);
                 }
             }
         );
 
         return view;
+    }
+
+    // Method to handle focus change
+    public void focusChanged(View view) {
+        activityCommander.focusChanged(weightInput.getText().toString());
     }
 }
--- app/src/main/java/edu/dtcc/crobin57/fragments/MainActivity.java
+++ app/src/main/java/edu/dtcc/crobin57/fragments/MainActivity.java
@@ -1,11 +1,11 @@
 package edu.dtcc.crobin57.fragments;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-//import android.view.View;
+//import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
